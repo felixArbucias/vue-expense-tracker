1a065ceeb2e4e774b198117ec9760db09aad6a0d
FelixArbucias <91673165+nameYAY0@users.noreply.github.com>
2024-06-14 12:06:53 -0400

    added toast

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -14,3 +14,31 @@
         <button class="btn">Add Transaction</button>
     </form>
 </template>
+<script setup>
+import { ref } from 'vue';
+import {useToast} from 'vue-toastification';
+const toast = useToast();
+
+const text = ref('');
+
+const amount = ref('');
+
+const emit = defineEmits(['transactionSubmitted']);
+
+const onSubmit = () => {
+    if (!text.value || !amount.value){
+        toast.error('Both fields must be filled');
+        return;
+    }
+
+
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value)
+    }
+    emit('transactionSubmitted', transactionData);
+    text.value = '';
+    amount.value = '';
+};
+
+</script>
